82b0c5be7375b3ecb2979a79526a257aa5f9a6f0
cxchhh <13601161004@163.com>
2024-06-08 10:32:20 +0800

    finish hw 14

--- debug/pai.sh
+++ debug/pai.sh
@@ -1,8 +1,11 @@
-target='XuanYu'
+target='21374067'
+idx=0
 while [ 1 ]
 do 
+    echo "round $idx:"
     python maker14.py ../static/workplace/users/$target/code.jar ./stdin.txt ./stdout.txt ./stderr.txt && python judge_unit4.py ./stdin.txt ./stdout.txt ./stderr.txt
     if [ $? -ne 0 ]; then
         break
     fi
+    let idx++
 done
